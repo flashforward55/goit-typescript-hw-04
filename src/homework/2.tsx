cc2ff43182f5d36b9ad47bde703211aa6d30e77d
Anton Styazhkin <rudana089@gmail.com>
2023-11-16 22:06:30 +0200

    Complete task1 & task2

--- src/homework/2.tsx
+++ src/homework/2.tsx
@@ -1,4 +1,17 @@
-import React, {useReducer} from "react";
+import React, { useReducer } from 'react';
+
+type RequestStep = 'idle' | 'start' | 'pending' | 'finished';
+
+type State = {
+  isRequestInProgress: boolean;
+  requestStep: RequestStep;
+};
+
+type Action =
+  | { type: 'START_REQUEST' }
+  | { type: 'PENDING_REQUEST' }
+  | { type: 'FINISH_REQUEST' }
+  | { type: 'RESET_REQUEST' };
 
 const initialState: State = {
   isRequestInProgress: false,
@@ -23,7 +36,7 @@ function requestReducer(state: State, action: Action): State {
 export function RequestComponent() {
   const [requestState, requestDispatch] = useReducer(requestReducer, initialState);
 
-  const startRequest = () => {
+  const startRequest = (): void => {
     requestDispatch({ type: 'START_REQUEST' });
     // Імітуємо запит до сервера
     setTimeout(() => {
@@ -35,7 +48,7 @@ export function RequestComponent() {
     }, 2000);
   };
 
-  const resetRequest = () => {
+  const resetRequest = (): void => {
     requestDispatch({ type: 'RESET_REQUEST' });
   };
 
